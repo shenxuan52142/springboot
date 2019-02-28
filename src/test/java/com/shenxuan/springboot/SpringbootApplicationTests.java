ffb8c3e64ccc01c171067e02a0581a5ed29ff20a
shenxuan <shenxuan1hao@163.com>
2019-02-28 21:45:17 +0800

    小修改

--- src/test/java/com/shenxuan/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/shenxuan/springboot/SpringbootApplicationTests.java
@@ -5,12 +5,40 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String var=null;
+		String path=null;
+		URL url = null;
+		try {
+			url = new URL(var);
+			DataInputStream dataInputStream = new DataInputStream(url.openStream());
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+			byte[] buffer = new byte[1024];
+			int length;
+
+			while ((length = dataInputStream.read(buffer)) > 0) {
+				output.write(buffer, 0, length);
+			}
+			fileOutputStream.write(output.toByteArray());
+			dataInputStream.close();
+			fileOutputStream.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
