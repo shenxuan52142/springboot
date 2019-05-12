3e721a528a974eff5d1c891082e7c7c687e15ade
shenxuan52142 <shenxuan1hao@163.com>
2019-05-12 22:19:55 +0800

    ....

--- src/main/java/com/shenxuan/springboot/gupiao/Gupiao.java
+++ src/main/java/com/shenxuan/springboot/gupiao/Gupiao.java
@@ -1,4 +1,175 @@
 package com.shenxuan.springboot.gupiao;
 
 public class Gupiao {
+    private String daima;
+    private String volume;
+    private double open;
+    private double high;
+    private double close;
+    private double low;
+    private String chg;
+    private String percent;
+    private float turnrate;
+    private float ma5;
+    private float ma10;
+    private float ma20;
+    private float ma30;
+    private String dif;
+    private String dea;
+    private double macd;
+    private String lot_volume;
+    private long timestamp;
+    private String time;
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public String getChg() {
+        return chg;
+    }
+
+    public void setChg(String chg) {
+        this.chg = chg;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
+    }
+
+    public float getTurnrate() {
+        return turnrate;
+    }
+
+    public void setTurnrate(float turnrate) {
+        this.turnrate = turnrate;
+    }
+
+    public float getMa5() {
+        return ma5;
+    }
+
+    public void setMa5(float ma5) {
+        this.ma5 = ma5;
+    }
+
+    public float getMa10() {
+        return ma10;
+    }
+
+    public void setMa10(float ma10) {
+        this.ma10 = ma10;
+    }
+
+    public float getMa20() {
+        return ma20;
+    }
+
+    public void setMa20(float ma20) {
+        this.ma20 = ma20;
+    }
+
+    public float getMa30() {
+        return ma30;
+    }
+
+    public void setMa30(float ma30) {
+        this.ma30 = ma30;
+    }
+
+    public String getDif() {
+        return dif;
+    }
+
+    public void setDif(String dif) {
+        this.dif = dif;
+    }
+
+    public String getDea() {
+        return dea;
+    }
+
+    public void setDea(String dea) {
+        this.dea = dea;
+    }
+
+    public double getMacd() {
+        return macd;
+    }
+
+    public void setMacd(double macd) {
+        this.macd = macd;
+    }
+
+    public String getLot_volume() {
+        return lot_volume;
+    }
+
+    public void setLot_volume(String lot_volume) {
+        this.lot_volume = lot_volume;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDaima() {
+        return daima;
+    }
+
+    public void setDaima(String daima) {
+        this.daima = daima;
+    }
 }
--- src/main/java/com/shenxuan/springboot/gupiao/Money.java
+++ src/main/java/com/shenxuan/springboot/gupiao/Money.java
@@ -1,7 +1,100 @@
 package com.shenxuan.springboot.gupiao;
 
-import java.util.List;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.shenxuan.springboot.IO.Use.TextFile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Money {
-    public static List<String> getG
+    public static void getAllGupiao() {
+        RestTemplate restTemplate = new RestTemplate();
+        String requestUrl = "https://xueqiu.com/stock/forchartk/stocklist.json";
+        //cookie
+        HttpHeaders headers = new HttpHeaders();
+        List<String> cookieList = new ArrayList<String>();
+        String cookie = "aliyungf_tc=AQAAABOjnjPFRQoAQ+PY3eSUpCbyyrUj; " +
+                "xq_a_token=62c76bcb2dcc3aa55d754d345301ac59fbd80d07; " +
+                "xq_a_token.sig=xanpxAxk9je8vsUD96Nzp5ZDpWE; " +
+                "xq_r_token=7adaa5963ce67bef34ec03e13ffae599835fa351; " +
+                "xq_r_token.sig=pP1yd0wp1nq2Ua_6Sl4lXmLql9g";
+        cookieList = Arrays.asList(cookie.split("; "));
+        headers.put(HttpHeaders.COOKIE, cookieList);
+        //请求参数
+        //当前时间戳
+        String var1 = TextFile.read("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\allgupiao.txt");
+        List<String> list = Arrays.asList(var1.split(","));
+        JSONArray allGupiao = new JSONArray();
+        JSONArray goodGupiao = new JSONArray();
+        List<String> goodGupiaoDaiMa = new ArrayList<>();
+        for (String s : list) {
+            long nowTime = System.currentTimeMillis();
+            MultiValueMap<String, String> param = new LinkedMultiValueMap<String, String>();//参数放入一个map中，restTemplate不能用hashMap
+            //将请求参数放入map中
+            param.add("symbol", s);
+            param.add("period", "1day");
+            param.add("type", "before");
+            param.add("begin", "1557244800000");
+            param.add("end", String.valueOf(nowTime));
+            param.add("_", String.valueOf(nowTime));
+            //System.out.println("参数内容为：" + param.toString());
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(param, headers);//将参数和header组成一个请求
+            JSONObject jsonObject = restTemplate.postForEntity(requestUrl, request, JSONObject.class).getBody();
+            JSONArray jsonArray = jsonObject.getJSONArray("chartlist");
+            //获取前一天0点的时间戳
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.set(calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH) - 2,
+                    0, 0, 0);
+            long beforeDay = calendar.getTimeInMillis() - 900;
+
+            for (int j = 0; j < jsonArray.size(); j++) {
+                Gupiao gupiao = JSONObject.parseObject(jsonArray.getJSONObject(j).toJSONString(), Gupiao.class);
+                if (beforeDay < gupiao.getTimestamp()) {
+                    gupiao.setDaima(s);
+                    allGupiao.add(gupiao);
+                    if (yiYangChuanSanXian(gupiao)) {
+                        goodGupiao.add(gupiao);
+                        goodGupiaoDaiMa.add(gupiao.getDaima());
+                    }
+                }
+            }
+
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String now = simpleDateFormat.format(new Date());
+        TextFile.write("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\" + now + "_juti.out", allGupiao.toJSONString());
+        TextFile.write("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\" + now + "zhiDeMaiDaiMa.out", String.join(",", goodGupiaoDaiMa));
+        TextFile.write("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\" + now + "zhiDeMai.out",goodGupiao.toJSONString());
+    }
+
+    public static void main(String[] args) {
+        getAllGupiao();
+    }
+
+    public static boolean yiYangChuanSanXian(Gupiao gupiao) {
+        boolean result = false;
+        if (compare(gupiao.getMa5(), gupiao.getMa10()) && compare(gupiao.getMa5(), gupiao.getMa20()) && compare(gupiao.getMa5(), gupiao.getMa30())) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static boolean compare(float a, float b) {
+        boolean result = false;
+        if (a / (a + b) > 0.4975 && a / (a + b) < 0.5025 && b / (a + b) > 0.4975 && b / (a + b) < 0.5025) {
+            result = true;
+        }
+        return result;
+    }
 }
