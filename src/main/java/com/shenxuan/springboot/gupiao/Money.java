17f6182b41d6ef55d8f5e74e115cd66db09b6818
shenxuan52142 <shenxuan1hao@163.com>
2019-05-14 00:31:23 +0800

    ....

--- src/main/java/com/shenxuan/springboot/gupiao/Money.java
+++ src/main/java/com/shenxuan/springboot/gupiao/Money.java
@@ -21,11 +21,7 @@ public class Money {
         //cookie
         HttpHeaders headers = new HttpHeaders();
         List<String> cookieList = new ArrayList<String>();
-        String cookie = "aliyungf_tc=AQAAABOjnjPFRQoAQ+PY3eSUpCbyyrUj; " +
-                "xq_a_token=62c76bcb2dcc3aa55d754d345301ac59fbd80d07; " +
-                "xq_a_token.sig=xanpxAxk9je8vsUD96Nzp5ZDpWE; " +
-                "xq_r_token=7adaa5963ce67bef34ec03e13ffae599835fa351; " +
-                "xq_r_token.sig=pP1yd0wp1nq2Ua_6Sl4lXmLql9g";
+        String cookie = "aliyungf_tc=AQAAABOjnjPFRQoAQ+PY3eSUpCbyyrUj; xq_a_token=a9632c244e3abdabf5e69648484ace358f551cc2; xq_a_token.sig=Jts98RL77SW_icc7iqdEtUiImZc; xq_r_token=2963a1e5c3d6891a6abd22a0205a91b4e1f78967; xq_r_token.sig=Um9wCJ4pmQ3kt_DnEGLCO5ykd6I";
         cookieList = Arrays.asList(cookie.split("; "));
         headers.put(HttpHeaders.COOKIE, cookieList);
         //请求参数
@@ -54,7 +50,7 @@ public class Money {
             calendar.setTime(new Date());
             calendar.set(calendar.get(Calendar.YEAR),
                     calendar.get(Calendar.MONTH),
-                    calendar.get(Calendar.DAY_OF_MONTH) - 2,
+                    calendar.get(Calendar.DAY_OF_MONTH) - 1,
                     0, 0, 0);
             long beforeDay = calendar.getTimeInMillis() - 900;
 
@@ -79,12 +75,12 @@ public class Money {
     }
 
     public static void main(String[] args) {
-        getAllGupiao();
+        zuixin();
     }
 
     public static boolean yiYangChuanSanXian(Gupiao gupiao) {
         boolean result = false;
-        if (compare(gupiao.getMa5(), gupiao.getMa10()) && compare(gupiao.getMa5(), gupiao.getMa20()) && compare(gupiao.getMa5(), gupiao.getMa30())) {
+        if (compare(gupiao.getMa5(), gupiao.getMa10()) && compare(gupiao.getMa5(), gupiao.getMa20())) {
             result = true;
         }
         return result;
@@ -92,9 +88,26 @@ public class Money {
 
     public static boolean compare(float a, float b) {
         boolean result = false;
-        if (a / (a + b) > 0.4975 && a / (a + b) < 0.5025 && b / (a + b) > 0.4975 && b / (a + b) < 0.5025) {
+        if (a / (a + b) > 0.4925 && a / (a + b) < 0.5075 && b / (a + b) > 0.4925 && b / (a + b) < 0.5025) {
             result = true;
         }
         return result;
+    }
+    public static void zuixin(){
+        String var1 = TextFile.read("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\2019-05-13_juti.out");
+        JSONArray jsonArray = JSONArray.parseArray(var1);
+        JSONArray jsonArray1 = new JSONArray();
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            Gupiao gupiao = JSONObject.parseObject(jsonArray.getJSONObject(i).toJSONString(),Gupiao.class);
+            if (yiYangChuanSanXian(gupiao)&&(gupiao.getClose()<gupiao.getMa5()&&gupiao.getClose()<gupiao.getMa10())){
+                jsonArray1.add(gupiao);
+                strings.add(gupiao.getDaima());
+            }
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String now = simpleDateFormat.format(new Date());
+        TextFile.write("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\" + now + "_guanzhu.out", jsonArray1.toJSONString());
+        TextFile.write("C:\\Users\\Administrator\\Desktop\\gupiaozuixin\\" + now + "guzhuDaiMa.out", String.join(",", strings));
     }
 }
